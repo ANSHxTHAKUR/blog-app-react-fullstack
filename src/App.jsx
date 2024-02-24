9d4acee7f68743e0b685481befa7af76bc8012bd
ANSHxTHAKUR <sengaransh094@gmail.com>
2024-02-24 15:07:11 +0530

    Project initialization

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+
+  return (
+    <>
+     <h1 className='bg-gray-700 text-yellow-400 text-center m-5 p-5 max-w-full'>Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
